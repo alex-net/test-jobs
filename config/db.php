8a95692e5603f88ccc688f9ac9c1948976f32024
Alex Net <alex-net-2004@ya.ru>
2025-12-09 16:22:42 +0300

    Переделали тестовое на использование mariadb вместо postgresql

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' =>  ENV_DATA['DB_DSN'] ?? 'mysql:host=localhost;dbname=yii2basic',
     'username' => ENV_DATA['DB_USER'] ?? 'root',
-    'password' => '',
+    'password' => ENV_DATA['DB_PASS'] ?? '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
